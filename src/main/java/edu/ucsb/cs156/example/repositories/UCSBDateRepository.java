55138ed2352d895701ee3655f5f2aa172cf6ca83
Phill Conrad <pconrad@cs.ucsb.edu>
2025-12-16 16:08:21 -0800

    pc - add Spring Data Rest for Url, and make sure other controllers do NOT use Spring Data Rest

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDateRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDateRepository.java
@@ -2,10 +2,12 @@ package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.UCSBDate;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 /** The UCSBDateRepository is a repository for UCSBDate entities. */
 @Repository
+@RepositoryRestResource(exported = false)
 public interface UCSBDateRepository extends CrudRepository<UCSBDate, Long> {
   /**
    * This method returns all UCSBDate entities with a given quarterYYYYQ.
